b8b6912d1ec43d0e9be930744fe37bed882e4dff
Aldahir Mayorga <samg.25sep@gmail.com>
2023-03-13 17:53:15 -0600

    Se agrega function para editar cita, se crea metodo editarCita

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 /*** Variables ****/
-
+let editando;
 //Inputs
     const mascotaInput = document.querySelector('#mascota');
     const propietarioInput = document.querySelector('#propietario');
@@ -39,11 +39,15 @@
 
         agregarCita (cita){
             this.citas = [...this.citas, cita];
-            console.log('this.citas :>> ', this.citas);
+            //console.log('this.citas :>> ', this.citas);
         };
 
         eliminarCita(id){
             this.citas = this.citas.filter(cita => cita.id != id);
+        };
+
+        editarCita(citaActualizada){
+            this.citas = this.citas.map(cita=> cita.id === citaActualizada.id ? citaActualizada : cita);
         };
 
     }
@@ -124,9 +128,18 @@
                 btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
                 btnEliminar.innerHTML = `Eliminar <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-              </svg>
-              `;
-              btnEliminar.onclick= ()=>eliminarCita(id);
+                </svg>
+                `;
+                btnEliminar.onclick= ()=>eliminarCita(id);
+
+                //Crear Boton para editar Cita
+                const btnEditar = document.createElement('button');
+                btnEditar.classList.add('btn', 'btn-info');
+                btnEditar.innerHTML = `Editar <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                </svg>
+                `;
+                btnEditar.onclick= ()=>cargarEdicion(cita);
 
 
                 //Agregar los parrafos al divCita
@@ -137,6 +150,7 @@
                 divCita.appendChild(horaParrafo);
                 divCita.appendChild(sintomasParrafo);
                 divCita.appendChild(btnEliminar);
+                divCita.appendChild(btnEditar);
 
                 //Agregar divCita a contenedorCitas
                 contendorCitas.appendChild(divCita);
@@ -167,7 +181,7 @@
             Para eso se accede a la propiedad del objeto con citaObj[e.target.name]
         */
         citaObj[e.target.name] = e.target.value;
-        console.log('citaObj :>> ', citaObj);
+        //console.log('citaObj :>> ', citaObj);
     }
 
 
@@ -182,14 +196,32 @@
         if(mascota ==='' || propietario==='' || telefono ==='' || fecha ==='' || hora  ==='' || sintomas  ===''){
             ui.mostrarAlerta('Todos los campos son obligatorios', 'error');
             return;
-        }
+        };
 
-        //Generar un id unico en el objetoCita
+        if(editando){
+            //console.log('Edicion activada');
+            //Alerta de agregado correctamente
+            ui.mostrarAlerta('Editado correctamente', 'exito');
 
-        citaObj.id = Date.now();
+            //Pasar el objeto de la cita a edicion de la
+            adminCitas.editarCita({...citaObj});
 
-        //Creando una nueva cita
-        adminCitas.agregarCita({...citaObj});
+             //Cambiar el texto del botton
+            formulario.querySelector('button[type="submit"]').textContent = 'Crear Cita';
+
+            //Quitar modo edicion
+            editando = false;
+        }else{
+            //Generar un id unico en el objetoCita
+            citaObj.id = Date.now();
+
+            //Creando una nueva cita
+            adminCitas.agregarCita({...citaObj});
+
+            //Alerta de agregado correctamente
+            ui.mostrarAlerta('Agregado correctamente', 'exito');
+        };
+
 
 
         //Reiniciar objeto para validacion
@@ -221,15 +253,41 @@
         //Eliminar cita
         adminCitas.eliminarCita(id);
 
-
         //Muestre un mensaje
         ui.mostrarAlerta('La cita se eliminó correctamente', 'exito');
-
 
         //Refresque contendorCitas
         ui.mostrarCitas(adminCitas);
 
-    }
+    };
+
+    //Carga los datos y el modo edición
+    function cargarEdicion(cita){
+        
+        const {mascota, propietario, telefono, fecha, hora, sintomas, id} = cita;
+        
+        //Llenar inputs para editar
+        mascotaInput.value = mascota;
+        propietarioInput.value = propietario;
+        telefonoInput.value = telefono;
+        fechaInput.value = fecha;
+        horaInput.value = hora;
+        sintomasInput.value = sintomas;
+
+         //Llenar el objeto 
+       citaObj.mascota = mascota;
+       citaObj.propietario = propietario;
+       citaObj.telefono = telefono;
+       citaObj.fecha = fecha;
+       citaObj.hora = hora;
+       citaObj.sintomas = sintomas;
+       citaObj.id = id;
+
+        //Cambiar el texto del botton
+        formulario.querySelector('button[type="submit"]').textContent = 'Guardar Cambios';
+
+        editando = true;
+    };
 
 /* Objetos */
 
