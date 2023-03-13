2b0456dbe1e7ddd8bd79a5c7a172bb1fb01024ba
Aldahir Mayorga <samg.25sep@gmail.com>
2023-03-13 16:47:25 -0600

    Se agrega function para eliminar cita, se crea metodo eliminarCita

--- js/app.js
+++ js/app.js
@@ -40,7 +40,11 @@
         agregarCita (cita){
             this.citas = [...this.citas, cita];
             console.log('this.citas :>> ', this.citas);
-        }
+        };
+
+        eliminarCita(id){
+            this.citas = this.citas.filter(cita => cita.id != id);
+        };
 
     }
 
@@ -76,7 +80,7 @@
 
         mostrarCitas({citas}){
             this.limpiarHTML();
-            
+
             citas.forEach(cita => {
                 //Extraer informacion del objeto de citas
                 const {mascota, propietario, telefono, fecha, hora, sintomas, id} = cita;
@@ -115,14 +119,24 @@
                     <span class="font-weight-bolder">sintomas: </span> ${sintomas}
                 `;
 
+                //Crear Boton para eliminar Cita
+                const btnEliminar = document.createElement('button');
+                btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
+                btnEliminar.innerHTML = `Eliminar <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+              </svg>
+              `;
+              btnEliminar.onclick= ()=>eliminarCita(id);
+
+
                 //Agregar los parrafos al divCita
                 divCita.appendChild(mascotaParrafo);
-
                 divCita.appendChild(propietarioParrafo);
                 divCita.appendChild(telefonoParrafo);
                 divCita.appendChild(fechaParrafo);
                 divCita.appendChild(horaParrafo);
                 divCita.appendChild(sintomasParrafo);
+                divCita.appendChild(btnEliminar);
 
                 //Agregar divCita a contenedorCitas
                 contendorCitas.appendChild(divCita);
@@ -200,6 +214,21 @@
         citaObj.hora = '';
         citaObj.sintomas='';
        
+    }
+
+    //Eliminar cita
+    function eliminarCita(id){
+        //Eliminar cita
+        adminCitas.eliminarCita(id);
+
+
+        //Muestre un mensaje
+        ui.mostrarAlerta('La cita se eliminó correctamente', 'exito');
+
+
+        //Refresque contendorCitas
+        ui.mostrarCitas(adminCitas);
+
     }
 
 /* Objetos */
