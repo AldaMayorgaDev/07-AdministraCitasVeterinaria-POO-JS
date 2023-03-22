b716e26c940a4e12bf4919310fd9a3f7b8146c63
Aldahir Mayorga <samg.25sep@gmail.com>
2023-03-21 20:09:23 -0600

    Se reestructura el proyecto haciendo uso de Modulos en js

---
+++ js/selectores.js
@@ -0,0 +1,16 @@
+
+//Inputs
+    export const mascotaInput = document.querySelector('#mascota');
+    export const propietarioInput = document.querySelector('#propietario');
+    export const telefonoInput = document.querySelector('#telefono');
+    export const fechaInput = document.querySelector('#fecha');
+    export const horaInput = document.querySelector('#hora');
+    export const sintomasInput = document.querySelector('#sintomas');
+
+//Formulario
+    export const formulario = document.querySelector('#nueva-cita');
+
+//Contenedor Citas
+    export const contendorCitas = document.querySelector('#citas');
+
+    export const heading = document.querySelector('#administra');
